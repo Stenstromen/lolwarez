b7fdc11fc4cc8951e851d16157096c9dfe19eda6
Stenstromen <peppoji@gmail.com>
2022-08-04 17:24:04 +0200

    Fixed iOS preview bug

--- controllers/warez.controller.js
+++ controllers/warez.controller.js
@@ -6,7 +6,7 @@ const dec = require("../crypto/dec.crypto");
 const fileId = require("../uniqueid/fid.uniqueid");
 const getKey = require("../uniqueid/key.uniqueid");
 const tmp = "../filez/tmp/";
-const mkdirp = require("mkdirp")
+const mkdirp = require("mkdirp");
 
 function sendIndex(req, res) {
   res.render("index.ejs");
@@ -16,20 +16,28 @@ async function sendFile(req, res) {
   const request = req.params.id.split("-");
   const fileId = request[0];
   const key = request[1];
+  const iosPreview = req
+    .get("User-Agent")
+    .includes("facebookexternalhit/1.1 Facebot Twitterbot/1.0");
 
-  if (fileId === "favicon.ico" || undefined) {
+  if (iosPreview) {
+    res.render("share.ejs");
     return;
   } else {
-    const decryptedFile = await dec(fileId, key);
-    const fileToSend = path.resolve(decryptedFile);
-    res.download(fileToSend);
+    if (fileId === "favicon.ico" || undefined) {
+      return;
+    } else {
+      const decryptedFile = await dec(fileId, key);
+      const fileToSend = path.resolve(decryptedFile);
+      res.download(fileToSend);
 
-    setTimeout(() => {
-      fs.unlink(fileToSend, function (err) {
-        if (err) throw err;
-        console.log("File deleted!");
-      });
-    }, 2000);
+      setTimeout(() => {
+        fs.unlink(fileToSend, function (err) {
+          if (err) throw err;
+          console.log("File deleted!");
+        });
+      }, 2000);
+    }
   }
 }
 
